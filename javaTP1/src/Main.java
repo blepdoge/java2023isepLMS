29e4e56c7886e00570cafe43d3bff60c7d15fa0a
Louis-Marie <blep.doge@gmail.com>
2023-02-10 12:28:18 +0100

    suite exos

--- javaTP1/src/Main.java
+++ javaTP1/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name, please: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + ", I am your robot overlord.");
+        sc.close();
+        //plutot stylé
     }
+
+    public static void division(int a, int b) {
+        System.out.println(a / b);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
