6e78237d09810338d7a8b11137733361e06b8855
Louis-Marie <blep.doge@gmail.com>
2023-03-10 12:08:29 +0100

    maj mouvement rook

--- TP3echec/src/Rook.java
+++ TP3echec/src/Rook.java
@@ -17,10 +17,10 @@ public class Rook extends Pieces{
         int startRow = fromPos.row;
         int startCol = fromPos.col;
 
-        computeMoveLine(currColor, chess, sp, startRow, startCol, Math.min(8-startRow,8-startCol),1, 1);
-        computeMoveLine(currColor, chess, sp, startRow, startCol, Math.min(startRow,8-startCol), -1, 1);
-        computeMoveLine(currColor, chess, sp, startRow, startCol, Math.min(startRow,startCol), -1, -1);
-        computeMoveLine(currColor, chess, sp, startRow, startCol, Math.min(8-startRow,startCol), 1, -1);
+        computeMoveLine(currColor, chess, sp, startRow, startCol, Math.min(8-startRow,8-startCol),0, 1);
+        computeMoveLine(currColor, chess, sp, startRow, startCol, Math.min(startRow,8-startCol), -1, 0);
+        computeMoveLine(currColor, chess, sp, startRow, startCol, Math.min(startRow,startCol), 0, -1);
+        computeMoveLine(currColor, chess, sp, startRow, startCol, Math.min(8-startRow,startCol), 1, 0);
         return sp;
 
 
